198dd4e5bde2d6175a6b1c23e22cbb96cc2e3219
Nahuel Cialone <ncialone@gmail.com>
2023-04-28 19:36:29 -0300

    actualizacion de template

--- entrega_3/urls.py
+++ entrega_3/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from entrega_3.views import saludo, segunda_vista  #Para acceder a la vista hay que importar el modulo y el método
+from entrega_3.views import saludo, segunda_vista, probandoTemplate  #Para acceder a la vista hay que importar el modulo y el método
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
     path('segunda_vista/',segunda_vista),
+    path('template/',probandoTemplate),
 ]
