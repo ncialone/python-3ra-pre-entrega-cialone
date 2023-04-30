4d7a370c5341af1f61970c03f570151507943afc
Nahuel Cialone <ncialone@gmail.com>
2023-04-30 14:34:45 -0300

    templates agregados con herencia y navbar unificado

--- App1/urls.py
+++ App1/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path 
-from App1 import views 
+from django.urls import path
+from App1 import views
 
-urlpatterns=[
-    path('', views.inicio),
-    path('cursos', views.cursos, name='Cursos'),
-    path('profesores', views.profesores),
-    path('estudiantes', views.estudiantes),
-    path('entregables', views.entregables),
+urlpatterns = [
+    path('', views.inicio, name="Inicio"), #este era nuestro primer view
+    path('cursos', views.cursos, name="Cursos"),
+    path('profesores', views.profesores, name="Profesores"),
+    path('estudiantes', views.estudiantes, name="Estudiantes"),
+    path('entregables', views.entregables, name="Entregables"),
 ]
